5ae57c99418a4f0ba80a5fba296cdd7c874812cd
roger-fpeng <pengfengmail@foxmail.com>
2026-03-05 13:38:10 +0800

    [~] gemm debug

--- include/afa_preset_kernels.cuh
+++ include/afa_preset_kernels.cuh
@@ -11,7 +11,9 @@ typedef void (*forward_kernel_fn)(const AFAForwardParams);
 std::map<AFAForwardKernelConfig, forward_kernel_fn>
     forward_kernels = {
         // (FP16, 128, 64, 32, 4): async+eager+swizzled+load_2_2_0_tiles
-        {AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, 2, 2, 0, false, false}, &afa_forward_kernel<AFAForwardKernelTraits<AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, false, false, 2, 2, 0,}>>},
+        // {AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, 2, 2, 0, false, false}, &afa_forward_kernel<AFAForwardKernelTraits<AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, false, false, 2, 2, 0,}>>},
+        {AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, 2, 2, 0, false, false}, &afa_forward_kernel<AFAForwardKernelTraits<AFAForwardKernelConfig{torch::kFloat16, 128, 64, 32, 4, true, true, true, false, false, 2, 2, 0}>>},
+    
     };
 } // namespace afa
 
